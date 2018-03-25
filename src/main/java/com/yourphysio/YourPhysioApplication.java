7a5e40de0b30f45520d670055680c5203a25ac84
pauloprescendo <paulovincius1994@hotmail.com>
2018-03-25 17:15:43 -0300

    first commit

---
+++ src/main/java/com/yourphysio/YourPhysioApplication.java
@@ -0,0 +1,12 @@
+package com.yourphysio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPhysioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourPhysioApplication.class, args);
+	}
+}
