4b01ef96ca4ccaecaded38a8407841e57e84e1ac
Rafael Evangelista <rafael.evans@live.com>
2018-05-18 15:35:14 -0300

    Incrementacao das rotas da pagina index

--- src/main/java/com/yourphysio/controller/IndexController.java
+++ src/main/java/com/yourphysio/controller/IndexController.java
@@ -1,14 +1,29 @@
 package com.yourphysio.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.yourphysio.model.User;
+import com.yourphysio.service.UserService;
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping("/")
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(method=RequestMethod.GET,value="/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/index")
+	public String index(User user) {
+		
+		userService.userRegister(user);
+		return "redirect:/index";
 	}
 
 }
